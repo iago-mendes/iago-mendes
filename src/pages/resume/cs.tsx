cb8287337fcdc2c1ad306aa3e2a23af352844762
Iago <iagobrazmendes@gmail.com>
2021-04-08 21:30:49 -0300

    create cs resume page

---
+++ src/pages/resume/cs.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+import Container from '../../styles/pages/resume/cs'
+
+const CSResume: React.FC = () =>
+{
+	return (
+		<Container>
+			<Head>
+				<title>Iago Mendes | Computer Science Resume</title>
+			</Head>
+		</Container>
+	)
+}
+
+export default CSResume

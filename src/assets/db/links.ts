598842374f2bc2a3ccf75d8b77eab61f0cf4c2f1
Iago <iagobrazmendes@gmail.com>
2021-07-30 19:25:38 -0300

    db: add links content

---
+++ src/assets/db/links.ts
@@ -0,0 +1,23 @@
+type Link = {
+	display: string
+	url: string
+	icon: string
+}
+
+export const links: Link[] = [
+	{
+		display: 'Star View (Google Play)',
+		url: 'https://play.google.com/store/apps/details?id=com.ufla.fsouza.starview',
+		icon: 'https://github.com/star-view.png'
+	},
+	{
+		display: 'cinephix.com',
+		url: 'https://cinephix.com',
+		icon: 'https://cinephix.com/favicon.svg'
+	},
+	{
+		display: 'Cinephix (Google Play)',
+		url: 'https://play.google.com/store/apps/details?id=com.cinephix.twa',
+		icon: 'https://cinephix.com/favicon.svg'
+	}
+]

aef7f69e0b93004fbe09b56cf881a120594061aa
Iago Mendes <iagobrazmendes@gmail.com>
2021-09-29 12:27:23 -0400

    feat: setup redirects

--- next.config.js
+++ next.config.js
@@ -11,4 +11,13 @@ module.exports = {
 			'letmeask.iago-mendes.me'
 		]
 	},
+	async redirects() {
+		return [
+			{
+				source: '/schedule',
+				destination: 'https://calendar.google.com/calendar/selfsched?sstoken=UUhidU9tZ0lmOUVyfGRlZmF1bHR8OGE1ZTFkM2Y2YTdlNzk5NDkwNjVhOTVlOWU3MDk4YjY',
+				permanent: true
+			}
+		]
+	}
 }

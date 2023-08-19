ba8a6bc2ad581a9ccf3eef8cda34369b2de4d071
Iago Mendes <ibrazmen@oberlin.edu>
2023-08-18 17:23:09 -0700

    Update background opacity.

--- src/styles/pages/index.ts
+++ src/styles/pages/index.ts
@@ -48,7 +48,7 @@ export const HomeContainer = styled.div`
 				width: 100%;
 				background: ${p => `linear-gradient(
 					to top,
-					${p.theme.colors.background}f2,
+					${p.theme.colors.background}E6,
 					${p.theme.colors.background}
 				)`};
 			}

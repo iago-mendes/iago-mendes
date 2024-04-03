e657a2fd90b621212e4fb1667be90d77fe1d4684
Iago Mendes <ibrazmen@oberlin.edu>
2024-04-03 11:26:35 -0700

    Update embedding links.

--- src/assets/db/links.ts
+++ src/assets/db/links.ts
@@ -5,13 +5,21 @@ type Link = {
 }
 
 export const embeddingLinks: Link[] = [
-	{text: 'Paper (in preparation)', url: '', disabled: true},
 	{
-		text: "Iago's Honors Thesis",
-		url: 'https://iagomendes.com/docs/embedding-thesis.pdf'
+		text: 'APS Talk Slides',
+		url: 'https://iagomendes.com/docs/embedding-aps-talk.pdf'
+	},
+	{
+		text: 'Embedding Videos',
+		url: 'https://drive.google.com/drive/folders/1OafAkZbVLxPKOJXkLtLlfjG4FMhhfkKG?usp=sharing'
 	},
 	{
 		text: 'FiDEC (open-source code)',
 		url: 'https://github.com/iago-mendes/fidec'
-	}
+	},
+	{
+		text: "Iago's Honors Thesis",
+		url: 'https://iagomendes.com/docs/embedding-thesis.pdf'
+	},
+	{text: 'Paper (in preparation)', url: '', disabled: true}
 ]
